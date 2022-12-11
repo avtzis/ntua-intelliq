9a865e9a568c9262e19d9e0ff55d307637cf2f3c
Altan Avtzi <avtzialtan@hotmail.com>
2022-12-11 16:52:44 +0200

    added https support and self-signed certificates

--- api-backend/server.js
+++ api-backend/server.js
@@ -1,5 +1,15 @@
 const app = require('./app');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
+const myServer = https.createServer(
+    {
+        key: fs.readFileSync(path.join(__dirname, './certificates', 'server.key')),
+        cert: fs.readFileSync(path.join(__dirname, './certificates', 'server.cert'))
+    },
+    app
+)
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+myServer.listen(port, () => console.log(`Server started on port ${port}`));
