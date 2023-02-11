c91c65446dd54be9ab5b48e496b4c2bd0339b652
ntua-el19241 <el19241@mail.ntua.gr>
2023-02-11 20:02:53 +0200

    fixed echo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ npm install
 USERNAME="root"
 read -p "MySQL Database username: (default: root) " USERNAME
 read -s -p "MySQL Database password: (default: <blank>) " PASSWORD
-echo "APP_ENV=staging\nDB_USER=$USERNAME\nDB_PASS=$PASSWORD" >> .env
+echo -e "APP_ENV=staging\nDB_USER=$USERNAME\nDB_PASS=$PASSWORD" >> .env
 
 cd ../cli/
 npm install
